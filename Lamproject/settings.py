c79f9d867accef5eb7e8d745cc95c0659693ef2b
Malickkk <93356892+Malickkk@users.noreply.github.com>
2022-03-03 10:17:35 -0400

    Update settings.py

--- Lamproject/settings.py
+++ Lamproject/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 from django.core.management.utils import get_random_secret_key
 import sys
-import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
